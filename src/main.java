128c068670b648813e072104adeaf1cf94066007
Алексей <erg163@ya.ru>
2025-05-16 14:05:02 +0400

    Ver 1.1

--- src/main.java
+++ src/main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class main {
+public class main {
+    public static void main(String[] args) {
+
+        int x = 13676; // стоимость билета
+        int y = 20; // количество рублей для одного бонуса
+        int miles = x/y;  // расчет милий
+        System.out.println(miles);
+
+    }
 }
