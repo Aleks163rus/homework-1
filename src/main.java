93aaabd83566b6e2f25c108a78ec2e33ae3538c9
Алексей <erg163@ya.ru>
2025-05-19 13:25:15 +0400

    Ver 1.2

--- src/main.java
+++ src/main.java
@@ -1,9 +1,10 @@
 public class main {
     public static void main(String[] args) {
 
-        int x = 13676; // стоимость билета
-        int y = 20; // количество рублей для одного бонуса
-        int miles = x/y;  // расчет милий
+        int price = 13676; // стоимость билета
+        int calculation = 20; // количество рублей для одного бонуса
+        int miles = price / calculation;  // расчет милий
+
         System.out.println(miles);
 
     }
